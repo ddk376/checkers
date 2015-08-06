385d729c247af4dbdc97b977c968f8a5a1e7f4aa
Gina <Gina@appacademy.io>
2015-08-06 17:32:06 -0400

    Add Game#won? Game#switch_player and Game#play.

--- game.rb
+++ game.rb
@@ -1,9 +1,28 @@
 require_relative 'board'
 class Game
   attr_reader :board
+  attr_accessor :players
 
   def initialize
     @board = Board.new
+    @players = {:white, :black}
   end
 
+  def play
+    until won?
+      system("clear")
+      board.render
+      play_turn
+      switch_player
+    end
+  end
+
+  def won?
+    board.all_pieces.all? { |el| el.color == :black } ||
+      board.all_pieces.all? {|el| el.color == :white}
+  end
+
+  def switch_player
+    players.rotate
+  end
 end
