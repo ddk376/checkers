f8568300bb7eecc57781f8830a4ec31cf46508cb
Gina <Gina@appacademy.io>
2015-08-06 10:20:35 -0400

    Add syntactic sugar for board class.

--- board.rb
+++ board.rb
@@ -1,8 +1,19 @@
 class Board
   BOARD_SIZE = 8
+  attr_reader :grid
 
   def initialize
 
+  end
+
+  def [](pos)
+    row, col = pos
+    @grid[row[col] = pos
+  end
+
+  def []=(pos,value)
+   row, col = pos
+   @grid[row][col] = value
   end
 
   def populate_grid
