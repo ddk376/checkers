f5ef2ced03c4918b890efddedbae234ce91e80e8
Gina <Gina@appacademy.io>
2015-08-06 17:36:35 -0400

    Add Game#current_player and Game#play_turn.

--- game.rb
+++ game.rb
@@ -17,12 +17,33 @@ class Game
     end
   end
 
+  def play_turn
+    begin
+      print "\n"
+      puts " #{current_player.to_s.capitalize}'s Turn:"
+      print "\n"
+      moves = get_move
+    rescue InvalidMoveError => e
+      system("clear")
+      board.render
+      print "\n"
+      puts e.message.colorize(:red)
+      retry
+    ensure
+      board.render
+    end
+  end
+
   def won?
     board.all_pieces.all? { |el| el.color == :black } ||
       board.all_pieces.all? {|el| el.color == :white}
   end
 
+  def current_player
+    players.first
+  end
+
   def switch_player
-    players.rotate
+    players.rotate!
   end
 end
