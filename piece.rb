ebb3a42ebb67991d2e1027cb47915684040fe424
Gina <Gina@appacademy.io>
2015-08-06 10:08:36 -0400

    Write skeleton of Piece class.

---
+++ piece.rb
@@ -0,0 +1,25 @@
+class Piece
+
+  def initialize
+
+  end
+
+  def perform_slide
+  end
+
+  def perform_jump #should remove the jumped piece from the Board
+
+  end
+
+  def illegal_slide?
+
+  end
+
+  def move_diffs  # returns the directions a piece could move in
+
+  end
+
+  def maybe_promote  #promote after each move which checks to see if a piece reached the back row
+    
+  end
+end
